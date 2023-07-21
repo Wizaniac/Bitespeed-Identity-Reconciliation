dc6a5dfdf2c7583c557ee34b6df5c92ec29af004
Tushar <tushar@easyecom.io>
2023-07-21 13:34:53 +0530

    Pushing Contact Model

---
+++ Models/Contact.js
@@ -0,0 +1,42 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const Contact = Sequelize.define('Contact', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  linkedId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  linkPrecedence: {
+    type: DataTypes.ENUM('secondary', 'primary'),
+    defaultValue: 'primary',
+    allowNull: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  deletedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+});
+
+module.exports = Contact;
